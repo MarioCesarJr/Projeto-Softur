a0adb673c45edc7612d696e1badfcfc9623ca863
Hever <hever.rodrigues2@gmail.com>
2014-09-17 22:10:19 -0300

    Classe cliente com os gets e sets e equals e hashCode

--- softur-projeto/src/softur/entities/Cliente.java
+++ softur-projeto/src/softur/entities/Cliente.java
@@ -12,10 +12,19 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
-import org.jboss.logging.LogMessage;
+
 @Entity
 public class Cliente implements Serializable {	
 
+		@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	
 		private static final long serialVersionUID = 1L;
 
 		@Id
@@ -39,5 +48,79 @@ public class Cliente implements Serializable {
 				
 		@OneToOne
 		private Endereco endereco;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getCpf() {
+			return cpf;
+		}
+
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+
+		public String getTelefone() {
+			return telefone;
+		}
+
+		public void setTelefone(String telefone) {
+			this.telefone = telefone;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public Date getDataCadastro() {
+			return dataCadastro;
+		}
+
+		public void setDataCadastro(Date dataCadastro) {
+			this.dataCadastro = dataCadastro;
+		}
+
+		public Endereco getEndereco() {
+			return endereco;
+		}
+
+		public void setEndereco(Endereco endereco) {
+			this.endereco = endereco;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Cliente other = (Cliente) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
+		}
+		
 		
 }
