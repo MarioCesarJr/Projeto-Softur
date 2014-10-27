930eeca0b553b29e977f948326a1468d48c5fc75
Mario jr <jun.soua.d@gmail.com>
2014-10-27 11:16:24 -0200

    mascara no formulario funcionario

---
+++ softur-projeto/WebContent/resources/js/mascara.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$(function() {
+		$("#telefone").mask("(99) 9999-9999");
+		$("#form\\:telefone").mask("(99) 9999-9999");
+		$("#cep").mask("99999-999");
+		$("#data").mask("99/99/9999");
+		$("#cpf").mask("999.999.999-99");
+		$("#cnpj").mask("99.999.999/9999-99");
+	});
+});
+
+$('#salario').priceFormat({
+	prefix : ' ',
+	centsSeparator : ',',
+	thousandsSeparator : '.'
+});
