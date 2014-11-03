77c9d1fd0f34d671a317f20c32fba8fd2ce8280e
Mario jr <jun.soua.d@gmail.com>
2014-11-03 14:59:53 -0200

    ajustes

--- softur-projeto/src/softur/main/funcionario/SalvandoFuncionario.java
+++ softur-projeto/src/softur/main/funcionario/SalvandoFuncionario.java
@@ -1,8 +1,12 @@
 package softur.main.funcionario;
 
-import java.util.Calendar;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import softur.entities.Cargo;
 import softur.entities.Endereco;
@@ -13,26 +17,34 @@ public class SalvandoFuncionario {
 
 	public static void main(String[] args) {
 
-		EntityManager em = null;
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("softurPU");
+		EntityManager em = emf.createEntityManager();
 		
 		Endereco endereco = new Endereco("SC", "Florianopolis", "88786098", "Rua Motta da Silva", "21", "estreito", "Brasil");
 		
+		Cargo cargo = new Cargo();
+		cargo.setId(1L);
+		
 		Funcionario funcionario = new Funcionario();
 		
-		funcionario.setNome("Ciclano da Silva");
-
-		Cargo cargo = new Cargo("Motorista");
-
-		
 		funcionario.setCargo(cargo);
+		
+		funcionario.setNome("Ciclano da Silva");
 		
 		funcionario.setCpf("05894987898");
 		
 		funcionario.setEndereco(endereco);
 		
-		Calendar dataEntrada = Calendar.getInstance();
-		dataEntrada.set(2010, 8, 21);
-//		funcionario.setDataAdmissao(dataEntrada);
+        Date data = new Date();
+		SimpleDateFormat formatarData = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			data = formatarData.parse("11/12/2012");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		funcionario.setDataAdmissao(data);
 				
 		funcionario.setEmail("ciclano@gmail.com");
 		
@@ -40,6 +52,12 @@ public class SalvandoFuncionario {
 		
 		funcionario.setSalario(2000d);
 		
-		Funcionarios dao = new Funcionarios();
+		Funcionarios dao = new Funcionarios(em);
+		
+		em.getTransaction().begin();
+		dao.salvar(funcionario);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
 	}
 }
